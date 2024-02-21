1d9ef2b9c8041cc32d9d167ab6d4bb74d0b0f630
saraelbana <matika20@gmail.com>
2024-02-21 16:53:15 +0100

    Finished app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
+import {fruitsInventory} from "./constants/FruitsInventory.js";
+import OrderForm from "./components/OrderForm.jsx";
 
 function App() {
-
   return (
-    <>
+    <div>
         <h1>Fruitmand bezorgservice</h1>
-    </>
-  )
+        <OrderForm fruitsInventory = {fruitsInventory}/>
+    </div>
+  );
 }
 
 export default App
