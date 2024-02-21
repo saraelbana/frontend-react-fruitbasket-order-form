884e6727815506e30ee85f492ce426fc04608348
saraelbana <matika20@gmail.com>
2024-02-21 16:23:14 +0100

    adding the reset button to order form

---
+++ src/components/OrderForm.jsx
@@ -0,0 +1,15 @@
+import FruitQuantitySelectorPanel from "./FruitQuantitySelectorPanel.jsx";
+import ResetButton from "./ResetButton.jsx";
+
+function OrderForm(fruitsInventory) {
+    return (
+       <ul>
+           {
+               fruitsInventory.map((fruit, index) => (<FruitQuantitySelectorPanel key={index} fruit={fruit} />))
+           }
+           <ResetButton />
+       </ul>
+    );
+}
+
+export default OrderForm;
