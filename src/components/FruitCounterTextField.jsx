a8b3bf504038a44a7b8f2689645bd1802cb95712
saraelbana <matika20@gmail.com>
2024-02-21 18:04:02 +0100

    Start creating the useState method

--- src/components/FruitCounterTextField.jsx
+++ src/components/FruitCounterTextField.jsx
@@ -1,6 +1,14 @@
-function FruitCounterTextField(counter){
+import {incrementCounter} from "../helpers/inventoryCalculations.js";
+
+import {useState} from "react";
+
+function FruitCounterTextField(fruitProps){
+    const [counter, setCounter] = useState(0);
     return(
-        <p>{counter}</p>
+   // {incrementCounter(displayCounter.value,fruitProps.inStock)}
+       <input type="number" id ="displayCounter" readOnly value={counter}>
+
+       </input>
     );
 }
 export default FruitCounterTextField;
