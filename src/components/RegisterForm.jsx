b546728eac6c1389cb8fb2af8dc488958fb7087d
saraelbana <matika20@gmail.com>
2024-02-22 07:55:22 +0100

    Adding multiple fields to the registr form

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,6 +1,13 @@
+import LabelInputPanel from "./LabelInputPanel.jsx";
+
 function RegisterForm(){
     return(
         <ul className="RegisterForm">
+            <LabelInputPanel value = "First Name"/>
+            <LabelInputPanel value = "Last Name"/>
+            <LabelInputPanel value = "Age"/>
+
         </ul>
     );
 }
+export default RegisterForm;
