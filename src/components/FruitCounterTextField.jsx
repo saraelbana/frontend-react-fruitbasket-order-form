2e8160de70b93ffcea42cd4a739ac82f458b5fac
saraelbana <matika20@gmail.com>
2024-02-22 01:11:33 +0100

    editing in the styling sheets for submitButton and ResetButton

--- src/components/FruitCounterTextField.jsx
+++
@@ -1,8 +0,0 @@
-function FruitCounterTextField(counter){
-    return(
-       <label type = "text" id ="displayCounter" >
-           {counter.counter}
-       </label>
-    );
-}
-export default FruitCounterTextField;
--- src/components/ImgIcon.jsx
+++
@@ -1,9 +0,0 @@
-import "./ImgIcon.css";
-function ImgIcon(iconProp){
-    return(
-        <label className= "ImgIcon">
-            {iconProp.icon}
-        </label>
-    );
-}
-export default ImgIcon;
